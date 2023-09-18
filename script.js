11b5cce6c7e48a1e7394d6c2e0f1beab6471d55e
Joseph Chu <karkinoscode@gmail.com>
2023-09-18 17:37:29 -0400

    Added API Key for NASA APOD

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
+// Variables
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imageContainer = document.querySelector(".images-container");
+const saveConfirm = document.querySelector(".saved-confirmed");
+const loader = document.querySelector(".loader");
+
 // NASA API
 const count = 10;
-const apiKey = "DEMO_KEY";
+const apiKey = "Et14ddK3OPcI23gA1bzg63Fch1bkjthU4kz3Ahaf";
 const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+
+function updateDOM() {
+  resultsArray.forEach((result) => {
+    // Card Container
+    const card = document.createElement("div");
+    card.classList.add("card");
+    // Link
+    const link = document.createElement("a");
+    link.href = result.hdURL;
+    link.title = "View Full Image";
+    link.target = "_blank";
+    // Image
+    const image = document.createElement("img");
+    image.src = result.url;
+    image.alt = "NASA Picture of the Day";
+    image.loading = "lazy";
+    image.classList.add("card-img-top");
+    // Card Body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    // Card Title
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = result.title;
+    // Saved Text
+    const saveText = document.createElement("p");
+    saveText.classList.add("clickable");
+    saveText.content = "Add to Favorites";
+    // Card Text
+    const cardText = document.createElement("p");
+    cardText.textContent = result.explanation;
+    // Footer Container
+    const footer = document.createElement("small");
+    footer.classList.add("text-muted");
+    // Date
+    const date = document.createElement("strong");
+    date.textContent = result.date;
+    // Copyright
+    const copyright = document.createElement("span");
+    copyright.textContent = ` ${result.copyright}`;
+    // Append
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    // console.log(card);
+    imagesContainer.appendChild(card);
+  });
+}
 
 // Get 10 images from NASA APOD API
 async function getNasaPictures() {
@@ -11,6 +67,7 @@ async function getNasaPictures() {
     const res = await fetch(apiURL);
     resultsArray = await res.json();
     console.log(resultsArray);
+    updateDOM();
   } catch (error) {
     // Catch Error Here
   }
